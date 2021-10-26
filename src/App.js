5f589acb1eb4ec45914d18e50027672bb263813f
Ashutosh31-pandey <ashutoshpandey.exe@gmail.com>
2021-10-26 23:49:55 +0530

    Created a component for searching the specific country

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
+import CitySelector from './Components/CitySelector';
 import WeatherCard from './Components/WeatherCard';
 import './App.css';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div className="App">
-      {/* dt is in unix-seconds but javascript uses milliseconds, multiply with 1000 */}
-      <WeatherCard
-        dt={1602104400 * 1000}
-        temp_min="22.67"
-        temp_max="24.39"
-        main="Clear"
-        icon="01d"
-      />
-      
-    </div>
+    <Container className = "App">
+      <CitySelector />
+    </Container>
   );
 };
 
--- src/Components/CitySelector.js
+++ src/Components/CitySelector.js
@@ -1,8 +1,39 @@
 import React, { useState } from "react";
+import { Row, Collapse, FormControl, Button, Col } from "react-bootstrap";
+
 
 const CitySelector = () => {
       const [city, setCity] = useState('');
-            return <div></div>;
+
+      return(
+            <>
+            <Row>
+                  <Col>
+                        <h1>Search your city</h1>
+                  </Col>
+            </Row>
+
+            <Row>
+                 {/* xs={4} takes the one third  of the page*/}
+                  <Col xs={4} className = "text-cemter">
+                        <FormControl
+                              placeholder ="Enter City"
+                              //update city value with the users input
+                              onChange = {(event) => setCity(event.target.value)}
+                              //value will be the currently selected city
+                              value = {city}
+                        />
+                        </Col> 
+            </Row>
+
+            <Row>
+                  <Col>
+                        {/* Event handler for button click */}
+                        <Button onClick = 'onSearch'>Check Weather</Button>
+                  </Col>
+            </Row>
+      </>
+      ); 
 };
 
 export default CitySelector;
